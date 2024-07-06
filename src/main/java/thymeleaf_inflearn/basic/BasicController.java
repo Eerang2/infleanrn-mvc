26daed810855a6c8bccd9f33f1e52557e8c1e421
Eerang2 <dlwlsdn8022@gmail.com>
2024-07-06 16:51:09 +0900

    김영한 강의 프로젝트 생성

---
+++ src/main/java/thymeleaf_inflearn/basic/BasicController.java
@@ -0,0 +1,11 @@
+package thymeleaf_inflearn.basic;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+}
